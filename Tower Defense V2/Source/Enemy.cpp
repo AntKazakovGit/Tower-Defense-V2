0d31677e81ddf790a41b636ec70f9ebddbd80f6d
Антон Казаков <anton_kazakov@outlook.com>
2018-03-28 15:33:07 +0300

    Добавлен класс Enemy

---
+++ Tower Defense V2/Source/Enemy.cpp
@@ -0,0 +1,12 @@
+﻿#include "Enemy.h"
+
+Enemy::Enemy(SDL_Texture * enemyTexture, int x, int y) :
+	Object(enemyTexture, x, y)
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
---
+++ Tower Defense V2/Source/Enemy.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "Object.h"
+
+
+class Enemy :
+	public Object
+{
+	// Здоровье противника
+	int health;
+public:
+	Enemy(SDL_Texture * enemyTexture, int x, int y);
+	~Enemy();
+};
