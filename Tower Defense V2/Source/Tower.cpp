452e77cc73e2943d39cdb7d2f62b36f5761cfdfc
Антон Казаков <anton_kazakov@outlook.com>
2018-03-28 15:40:34 +0300

    Добавлен класс Tower

---
+++ Tower Defense V2/Source/Tower.cpp
@@ -0,0 +1,11 @@
+﻿#include "Tower.h"
+
+Tower::Tower(SDL_Texture * towerTexture, int x, int y) :
+	Object(towerTexture, x, y)
+{
+
+}
+
+Tower::~Tower()
+{
+}
---
+++ Tower Defense V2/Source/Tower.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "Object.h"
+
+
+class Tower :
+	public Object
+{
+	// Урон башни
+	int damage;
+public:
+	Tower(SDL_Texture * towerTexture, int x, int y);
+	~Tower();
+};
