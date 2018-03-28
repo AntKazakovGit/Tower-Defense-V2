aa68d6c2959d89916aa8eabc6c806cc07c0bb10b
Антон Казаков <anton_kazakov@outlook.com>
2018-03-28 15:26:30 +0300

    Добавлено несколько внутриигровых параметров

--- Tower Defense V2/Source/Game.h
+++ Tower Defense V2/Source/Game.h
@@ -6,14 +6,25 @@
 class Game :
 	public Scene
 {
+	// Текстуры используемые в работе приложения
+	std::vector<SDL_Texture*> gameTextures;
+	// Здоровье крепости
 	int castleHealth;
+	// Золото героя
+	int gold;
+	// Карта уровня
+	Object * gameMap;
+	// Список врагов
+
+	// Список башен
+
 public:
 	Game(SDL_Renderer * renderer);
 	~Game();
-	// 
+	// Возвращает список отоброжаемых объектов
 	std::vector<Object*> GetDisplayedObjects();
-	// 
+	// Выполнение сцены
 	void Execution();
-	// Переопределение Scene::OnMouseClick
+	// Обработка нажатия мыши
 	void OnMouseClick(SDL_MouseButtonEvent buttonEvent);
 };
